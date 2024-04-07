4cd9483d9cf98e321fb14ac3e37106e9d3ced9f7
max <29782397+tekoh@users.noreply.github.com>
2024-04-07 16:30:56 +0100

    review page

---
+++ src/routes/dashboard/review/+page.server.ts
@@ -0,0 +1,18 @@
+import db from "$lib/server/database/database.js";
+import { images } from "$lib/server/database/schema.js";
+import { redirect } from "@sveltejs/kit";
+import { count, eq } from "drizzle-orm";
+
+export async function load({ parent }) {
+  const { user } = await parent();
+
+  if (user.type === "user") return redirect(302, "/dashboard");
+
+  return {
+    categories: await db
+      .select({ type: images.type, amount: count() })
+      .from(images)
+      .where(eq(images.verified, 0))
+      .groupBy(images.type),
+  };
+}
---
+++ src/routes/dashboard/review/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let data;
+</script>
+
+<h2 class="ml-4 text-3xl font-medium text-primary">Images for review</h2>
+{#if data.categories.length > 0}
+  <div class="mt-14 grid w-full grid-cols-3 gap-4">
+    {#each data.categories as category}
+      <a
+        class=" rounded-lg border border-secondary bg-secondary bg-opacity-20 p-4 text-center duration-300 hover:opacity-75"
+        href="/dashboard/review/{encodeURIComponent(category.type)}"
+      >
+        <h3 class=" text-xl font-medium text-primary">{category.type}</h3>
+        <p class="mt-2 text-accent">
+          {category.amount.toLocaleString()} image{category.amount > 1 ? "s" : ""}
+        </p>
+      </a>
+    {/each}
+  </div>
+{:else}
+  <p>No images to review</p>
+{/if}
