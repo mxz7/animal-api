d867b20a1841079aa0d6e948fb7b0d5958546439
max <29782397+tekoh@users.noreply.github.com>
2024-04-07 22:00:54 +0100

    better floating nav bar on mobile

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -3,17 +3,19 @@
 </script>
 
 <header class="flex w-full justify-center pt-4">
-  <nav
-    class="flex w-full max-w-4xl items-center gap-8 rounded-full bg-secondary bg-opacity-15 p-4 px-6 text-lg shadow"
-  >
-    <a href="/" class="hidden pr-8 text-2xl font-bold text-primary sm:block">animals</a>
-    <a href="/" class="sm:hidden">
-      <Home class="text-primary" strokeWidth={2.5} size={28} />
-    </a>
-    <a href="/docs">docs</a>
-    <div class="grow" />
-    <a href="/dashboard">
-      <CircleUser class="text-primary" strokeWidth={2.5} size={28} />
-    </a>
-  </nav>
+  <div class="w-full max-w-4xl px-4">
+    <nav
+      class="flex w-full items-center gap-8 rounded-full bg-secondary bg-opacity-15 p-4 text-lg shadow"
+    >
+      <a href="/" class="hidden pr-8 text-2xl font-bold text-primary sm:block">animals</a>
+      <a href="/" class="sm:hidden">
+        <Home class="text-primary" strokeWidth={2.5} size={28} />
+      </a>
+      <a href="/docs">docs</a>
+      <div class="grow" />
+      <a href="/dashboard">
+        <CircleUser class="text-primary" strokeWidth={2.5} size={28} />
+      </a>
+    </nav>
+  </div>
 </header>
