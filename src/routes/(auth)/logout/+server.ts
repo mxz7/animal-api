4d15c2b83c24ae67a3c7e2689fa813058bde3853
max <29782397+tekoh@users.noreply.github.com>
2024-04-08 23:28:42 +0100

    logout route

---
+++ src/routes/(auth)/logout/+server.ts
@@ -0,0 +1,10 @@
+import { lucia } from "$lib/server/auth.js";
+import { redirect } from "@sveltejs/kit";
+
+export async function GET({ locals }) {
+  const auth = await locals.validate();
+
+  if (!auth) return redirect(302, "/");
+  lucia.invalidateSession(auth.session.id);
+  return redirect(302, "/");
+}
