70f5cc963c7bcf696a74479510054efeb683137a
max <29782397+tekoh@users.noreply.github.com>
2024-04-09 00:32:36 +0100

    better mobile styling

--- src/routes/dashboard/review/+page.svelte
+++ src/routes/dashboard/review/+page.svelte
@@ -7,10 +7,10 @@
 </svelte:head>
 
 {#if data.categories.length > 0}
-  <div class="mt-14 grid w-full grid-cols-3 gap-4">
+  <div class="mt-14 grid w-full grid-cols-1 gap-4 md:grid-cols-3">
     {#each data.categories as category}
       <a
-        class=" rounded-lg border border-secondary bg-secondary bg-opacity-20 p-4 text-center duration-300 hover:opacity-75"
+        class="rounded-lg border border-secondary bg-secondary bg-opacity-20 p-4 text-center duration-300 hover:opacity-75"
         href="/dashboard/review/{encodeURIComponent(category.type)}"
       >
         <h3 class=" text-xl font-medium text-primary">{category.type}</h3>
