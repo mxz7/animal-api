94dd180da038ab90abce65554b8d9fc9b642f9cf
max <29782397+tekoh@users.noreply.github.com>
2024-04-09 00:26:23 +0100

    better styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
   <title>animals API</title>
 </svelte:head>
 
-<h1 class="mt-16 w-full text-center text-4xl font-bold text-primary sm:text-6xl">animals API</h1>
-<p class="text-center italic text-secondary">
+<h1 class="mt-16 w-full text-center text-5xl font-bold text-primary md:text-6xl">animals API</h1>
+<p class="text-center text-sm italic text-secondary">
   served {#await data.served}
     ...
   {:then served}
@@ -23,7 +23,7 @@
 
 <div class="mt-48 flex w-full justify-center">
   <div class="w-full max-w-4xl px-4 md:px-0">
-    <h2 class="text-xl font-medium text-primary">Supported Animal Types</h2>
+    <h2 class="text-2xl font-medium text-primary" id="supported">Supported Animal Types</h2>
     {#await data.categories}
       <div class="flex w-full justify-center">
         <span class="loading-spinner" />
@@ -35,13 +35,16 @@
             href="/{category.type}/random"
             class="mt-2 rounded-lg border border-secondary bg-secondary bg-opacity-20 p-4 text-center"
           >
-            <h3 class="text-xl font-medium text-primary">{category.type}</h3>
-            <p class="mt-2 text-accent">
+            <h3 class="text-2xl font-medium text-primary">{category.type}</h3>
+            <p class="mt-2 text-lg text-accent">
               {category.count.toLocaleString()} image{category.count > 1 ? "s" : ""}
             </p>
           </a>
         {/each}
       </div>
+      <a class="link mt-4 block w-full text-center text-secondary" href="/dashboard/upload">
+        Submit pictures of your own animals here
+      </a>
     {/await}
   </div>
 </div>
