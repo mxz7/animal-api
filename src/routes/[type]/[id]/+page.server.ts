ad33311a650cdd2446225908320561a66c6a6e80
max <29782397+tekoh@users.noreply.github.com>
2024-04-08 23:32:25 +0100

    delete from s3

--- src/routes/[type]/[id]/+page.server.ts
+++ src/routes/[type]/[id]/+page.server.ts
@@ -1,8 +1,14 @@
 import db from "$lib/server/database/database.js";
 import { imageLikes, images, users } from "$lib/server/database/schema.js";
+import { s3 } from "$lib/server/s3.js";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { fail } from "@sveltejs/kit";
 import { and, eq } from "drizzle-orm";
 import { nanoid } from "nanoid";
+
+export const config = {
+  runtime: "nodejs20.x",
+};
 
 export async function load({ locals }) {
   return { user: locals.validate().then((auth) => auth?.user) };
@@ -15,6 +21,9 @@ export const actions = {
     if (!auth || auth.user.type !== "admin") return fail(400);
 
     await db.delete(images).where(eq(images.id, `${params.type}/${params.id}`));
+    await s3.send(
+      new DeleteObjectCommand({ Bucket: "maxzdev-animals", Key: `${params.type}/${params.id}` }),
+    );
   },
   addLike: async ({ getClientAddress, params }) => {
     if (await banCheck(getClientAddress())) return fail(400);
