7646a098ca6ab1e69a84458f456a799b17db77fd
max <29782397+tekoh@users.noreply.github.com>
2024-04-09 21:26:01 +0100

    use tinybird analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,20 @@
 <script>
-  import { dev } from "$app/environment";
   import { page } from "$app/stores";
   import Footer from "$lib/components/footer.svelte";
   import Navbar from "$lib/components/navbar.svelte";
-  import { inject } from "@vercel/analytics";
   import { Toaster } from "svelte-french-toast";
   import "../app.css";
-
-  if (!dev) inject({ mode: "production" });
 </script>
 
 <svelte:head>
   <meta name="og:url" content={$page.url.toString()} />
+
+  <script
+    defer
+    src="https://unpkg.com/@tinybirdco/flock.js"
+    data-host="https://api.eu-central-1.aws.tinybird.co"
+    data-token="p.eyJ1IjogImVjMDk1Y2EwLWU2M2EtNDJmYy1iOTk2LWI5ZGM2MzUxMWZmMCIsICJpZCI6ICIyMzBlOTMwZS1mMzJjLTRlYmEtYjFlZS04NjU4OWJjYWZkNzIiLCAiaG9zdCI6ICJhd3MtZXUtY2VudHJhbC0xIn0.MEw55QckfzHZ1HHkZxbNxk7T6sTWlPHcyfVyH6EHyh0"
+  ></script>
 </svelte:head>
 
 <Toaster />
