278219e577a1a67fe2282f120803634d2f0bb511
max <29782397+tekoh@users.noreply.github.com>
2024-04-09 21:23:06 +0100

    try catch

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -67,11 +67,15 @@ export const handle = async ({ event, resolve }) => {
 
   const res = await resolve(event);
 
-  if (
-    (res.redirected || res.status === 404 || res.status === 500) &&
-    res.headers.get("cache-control")
-  )
-    res.headers.delete("cache-control");
+  try {
+    if (
+      (res.redirected || res.status === 404 || res.status === 500) &&
+      res.headers.get("cache-control")
+    )
+      res.headers.delete("cache-control");
+  } catch {
+    // do nothing lol
+  }
 
   return res;
 };
