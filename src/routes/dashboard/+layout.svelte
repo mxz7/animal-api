6afabaa4e92aab75218d4bd2893ebbba387e63fe
max <29782397+tekoh@users.noreply.github.com>
2024-04-10 20:10:51 +0100

    mobile can hide nav

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { ArrowLeft, ArrowRightFromLine } from "lucide-svelte";
 
   export let data;
+  let showNav = false;
 
   const routes: { href: string; text: string; perms?: ("mod" | "admin")[] }[] = [
     { href: "/dashboard/images", text: "images" },
@@ -14,7 +16,11 @@
   <div class="w-full px-4 md:max-w-3xl md:px-0">
     <!-- <h1 class="text-center text-3xl font-semibold text-primary">animals dashboard</h1> -->
     <div class=" flex w-full gap-8">
-      <div class="flex flex-col gap-3 rounded-sm border-r border-secondary py-2 pr-4">
+      <div
+        class="{showNav
+          ? 'flex'
+          : 'hidden'} flex-col gap-3 rounded-sm border-r border-secondary py-2 pr-4 md:flex"
+      >
         {#each routes as { href, text, perms }}
           {#if perms}
             {#each perms as perm}
@@ -33,16 +39,26 @@
           {/if}
         {/each}
       </div>
+
       <div class="grow">
-        <div class="breadcrumbs pb-4 text-sm">
-          <ul>
-            {#each $page.url.pathname.split("/") as path, i}
-              {#if path}
-                {@const paths = $page.url.pathname.split("/")}
-                <li><a href="/{paths.slice(1, i + 1).join('/')}">{path}</a></li>
-              {/if}
-            {/each}
-          </ul>
+        <div class=" flex items-center gap-2 pb-4">
+          <button class="-ml-2 md:hidden" on:click={() => (showNav = !showNav)}>
+            {#if showNav}
+              <ArrowLeft />
+            {:else}
+              <ArrowRightFromLine />
+            {/if}
+          </button>
+          <div class="breadcrumbs text-sm">
+            <ul>
+              {#each $page.url.pathname.split("/") as path, i}
+                {#if path}
+                  {@const paths = $page.url.pathname.split("/")}
+                  <li><a href="/{paths.slice(1, i + 1).join('/')}">{path}</a></li>
+                {/if}
+              {/each}
+            </ul>
+          </div>
         </div>
         <slot />
       </div>
