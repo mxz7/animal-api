0e2acd48568342bf60329db2b8229115558409ac
max <29782397+tekoh@users.noreply.github.com>
2024-04-12 00:24:51 +0100

    Revert "try moving out of component"
    This reverts commit 3e868b61dbc588c42943ff1f1f4d9d131866998b.

--- src/routes/[type]/[id]/likeButton.svelte
+++ src/routes/[type]/[id]/likeButton.svelte
@@ -4,3 +4,38 @@
   import toast from "svelte-french-toast";
 </script>
 
+<form
+  action="?/like"
+  method="POST"
+  use:enhance={() => {
+    let done = 0;
+
+    toast.promise(
+      new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+          if (done === 1) {
+            resolve(1);
+            clearInterval(interval);
+          } else if (done === 2) {
+            reject(2);
+            clearInterval(interval);
+          }
+        });
+      }),
+      { error: "Failed to add like", success: "Added like", loading: "Adding like..." },
+    );
+
+    return ({ result }) => {
+      console.log(result);
+      if (result.status !== 200) {
+        done = 2;
+      } else {
+        done = 1;
+      }
+    };
+  }}
+>
+  <button class="btn btn-ghost tooltip flex items-center" data-tip="Like">
+    <ThumbsUp size={20} />
+  </button>
+</form>
