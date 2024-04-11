b5a905a4cd803c3d795fa4717219f02dceecaba7
max <29782397+tekoh@users.noreply.github.com>
2024-04-11 02:29:22 +0100

    dont fetch from edge on serverless

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import { ISR_TOKEN } from "$env/static/private";
+import { PUBLIC_CDN_URL, PUBLIC_URL } from "$env/static/public";
 import db from "$lib/server/database/database.js";
-import { requests } from "$lib/server/database/schema.js";
-import type { Image, Types } from "$lib/types/api.js";
-import { asc, sum } from "drizzle-orm";
+import { imageLikes, imageReports, images, requests } from "$lib/server/database/schema.js";
+import type { Types } from "$lib/types/api.js";
+import { and, asc, count, eq, sum } from "drizzle-orm";
 
 export const config = {
   isr: {
@@ -26,7 +27,34 @@ export async function load({ request, fetch }) {
     .then((r) => r[0]?.date ?? 0);
 
   const categories = fetch("/api/types").then((r) => r.json() as Promise<Types>);
-  const cat = fetch("/api/cat/random").then((r) => r.json() as Promise<Image>);
+  const cat = (async () => {
+    const [{ amount }] = await db
+      .select({ amount: count(images.id) })
+      .from(images)
+      .where(eq(images.type, "cat"));
+
+    const [query] = await db
+      .select({
+        id: images.id,
+        name: images.name,
+        type: images.type,
+        likes: count(imageLikes.id),
+        reports: count(imageReports.id),
+      })
+      .from(images)
+      .where(and(eq(images.verified, 1), eq(images.type, "cat")))
+      .leftJoin(imageLikes, eq(images.id, imageLikes.imageId))
+      .leftJoin(imageReports, eq(images.id, imageReports.imageId))
+      .groupBy(images.id)
+      .limit(1)
+      .offset(Math.floor(Math.random() * amount));
+
+    return {
+      ...query,
+      url: `${PUBLIC_URL}/${query.type}/${query.id}`,
+      image: `${PUBLIC_CDN_URL}/${query.type}/${query.id}`,
+    };
+  })(); // this cannot be the api route because serverless -> edge isnt good for some reason
 
   if (request.headers.get("user-agent")?.toLowerCase().includes("bot")) {
     return {
