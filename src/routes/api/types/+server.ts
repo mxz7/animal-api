a4f665b407f9c1ec95ba7528c2c989c6293d9aad
max <29782397+tekoh@users.noreply.github.com>
2024-04-12 12:15:05 +0100

    order

--- src/routes/api/types/+server.ts
+++ src/routes/api/types/+server.ts
@@ -2,7 +2,7 @@ import { ISR_TOKEN } from "$env/static/private";
 import db from "$lib/server/database/database.js";
 import { images } from "$lib/server/database/schema.js";
 import { json } from "@sveltejs/kit";
-import { eq, sql } from "drizzle-orm";
+import { count, desc, eq, sql } from "drizzle-orm";
 
 export const config = {
   isr: {
@@ -16,7 +16,8 @@ export async function GET() {
     .select({ type: images.type, count: sql`count()` })
     .from(images)
     .groupBy(images.type)
-    .where(eq(images.verified, 1));
+    .where(eq(images.verified, 1))
+    .orderBy(desc(count()));
 
   return json(query);
 }
