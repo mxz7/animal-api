4554e6bdf9931ff118487e3d9dad688dd393b0cf
max <29782397+tekoh@users.noreply.github.com>
2024-05-22 16:22:24 +0100

    update github username

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,7 +4,7 @@
 
 <footer class="footer footer-center bg-base-300 p-4">
   <div class="flex justify-center gap-4">
-    <a href="https://github.com/tekoh/animal-api" target="_blank">
+    <a href="https://github.com/mxz7/animal-api" target="_blank">
       <Github size={32} class="text-secondary" />
     </a>
   </div>
