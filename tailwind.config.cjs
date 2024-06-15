d0ed13231603175102782247741d7d7b60b41244
max <29782397+tekoh@users.noreply.github.com>
2024-06-15 22:36:37 +0100

    dark and light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,19 +3,8 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   daisyui: {
-    themes: ["night"],
-  },
-
-  theme: {
-    extend: {
-      colors: {
-        // text: "#f4e6e9",
-        // background: "#1b080c",
-        // primary: "#EC3C62",
-        // secondary: "#8c172e",
-        // accent: "#E68EA0",
-      },
-    },
+    themes: ["winter", "night"],
+    darkTheme: "night",
   },
 
   plugins: [require("daisyui")],
