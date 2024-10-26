6fc9a1ac6b858d65875f6a454b6aa68893645d9b
max <29782397+mxz7@users.noreply.github.com>
2024-10-26 16:24:52 +0100

    remove - from nanoid alphabet

--- src/lib/nanoid.ts
+++ src/lib/nanoid.ts
@@ -1,6 +1,6 @@
 import { customAlphabet } from "nanoid";
 
 export const nanoid = customAlphabet(
-  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-",
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz",
   7,
 );
