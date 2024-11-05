8276d38a3fc507e23ea1d2a440645e42d36f28ed
max <29782397+mxz7@users.noreply.github.com>
2024-11-05 17:45:11 +0000

    better structure

--- src/routes/[type]/[id]/+page.svelte
+++ src/routes/[type]/[id]/+page.svelte
@@ -36,41 +36,36 @@
   <meta property="og:type" content="image" />
 </svelte:head>
 
-<div class="mt-8 flex w-full justify-center">
-  <div class="w-full md:w-full md:max-w-4xl">
-    <div class="flex w-fit flex-col-reverse justify-center gap-4 md:w-full md:flex-row">
-      <div class="px-4 md:w-2/5 md:px-0">
-        <img
-          class="w-full rounded-lg"
-          src="https://r2.animals.maxz.dev/{data.image.type}/{data.image.id}"
-          alt={data.image.name}
-          decoding="async"
-          fetchpriority="high"
-          loading="eager"
-        />
-      </div>
-      <div class="px-4">
-        <h1
-          class="font-semibold text-primary {data.image.name
-            ? 'text-2xl md:text-3xl'
-            : 'md:text-lg'}"
-        >
-          {data.image.name || data.image.id}
-        </h1>
-        {#if data.image.uploaderUsername}
-          <h2 class="mb-3 text-sm">by {data.image.uploaderUsername}</h2>
+<div class="mt-8 flex w-full justify-center px-4">
+  <div class="flex w-full flex-col-reverse gap-4 md:w-full md:max-w-4xl md:grid-cols-2 md:flex-row">
+    <img
+      class="rounded-lg md:w-0 md:grow md:basis-0"
+      src="https://r2.animals.maxz.dev/{data.image.type}/{data.image.id}"
+      alt={data.image.name}
+      decoding="async"
+      fetchpriority="high"
+      loading="eager"
+    />
+
+    <div class="md:w-0 md:grow md:basis-0">
+      <h1
+        class="font-semibold text-primary {data.image.name ? 'text-2xl md:text-3xl' : 'md:text-lg'}"
+      >
+        {data.image.name || data.image.id}
+      </h1>
+      {#if data.image.uploaderUsername}
+        <h2 class="mb-3 text-sm">by {data.image.uploaderUsername}</h2>
+      {/if}
+
+      <p>Uploaded: {dayjs(data.image.createdAt).format("YYYY-MM-DD")}</p>
+      <p>{data.image.likes.toLocaleString()} like{data.image.likes === 1 ? "" : "s"}</p>
+      <div class="mt-4 flex gap-4">
+        <LikeButton addLike={() => data.image.likes++} />
+        <ReportButton reportForm={data.reportForm} />
+
+        {#if admin}
+          <DeleteButton />
         {/if}
-
-        <p>Uploaded: {dayjs(data.image.createdAt).format("YYYY-MM-DD")}</p>
-        <p>{data.image.likes.toLocaleString()} like{data.image.likes === 1 ? "" : "s"}</p>
-        <div class="mt-4 flex gap-4">
-          <LikeButton addLike={() => data.image.likes++} />
-          <ReportButton reportForm={data.reportForm} />
-
-          {#if admin}
-            <DeleteButton />
-          {/if}
-        </div>
       </div>
     </div>
   </div>
